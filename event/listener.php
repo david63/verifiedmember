3399e5f7662f86aa4e214ccf420f7276bf029d7e
David Wood <david.wood@pear-tree.org.uk>
2021-03-01 14:15:16 +0000

    RC2

--- event/listener.php
+++ event/listener.php
@@ -18,6 +18,7 @@ use phpbb\request\request;
 use phpbb\db\driver\driver_interface;
 use phpbb\language\language;
 use phpbb\template\template;
+use phpbb\group\helper;
 use david63\verifiedmember\core\functions;
 
 /**
@@ -37,6 +38,9 @@ class listener implements EventSubscriberInterface
 	/** @var template */
 	protected $template;
 
+	/** @var helper */
+	protected $group_helper;
+
 	/** @var functions */
 	protected $functions;
 
@@ -53,18 +57,20 @@ class listener implements EventSubscriberInterface
 	 * @param driver_interface  $db				Db object
 	 * @param language          $language		Language object
 	 * @param template          $template		Template object
+	 * @param helper			$group_helper	Group helper object
 	 * @param functions         $functions		Functions for the extension
 	 * @param array             $tables			phpBB db tables
 	 * @param string            $images_path    Path to this extension's images
 	 *
 	 * @access public
 	 */
-	public function __construct(request $request, driver_interface $db, language $language, template $template, functions $functions, array $tables, string $images_path)
+	public function __construct(request $request, driver_interface $db, language $language, template $template, helper $group_helper, functions $functions, array $tables, string $images_path)
 	{
 		$this->request		= $request;
 		$this->db			= $db;
 		$this->language		= $language;
 		$this->template		= $template;
+		$this->group_helper	= $group_helper;
 		$this->functions	= $functions;
 		$this->tables		= $tables;
 		$this->images_path	= $images_path;
@@ -80,12 +86,37 @@ class listener implements EventSubscriberInterface
 	public static function getSubscribedEvents()
 	{
 		return [
-			'core.acp_manage_group_request_data'		=> 'add_group',
-			'core.acp_manage_group_initialise_data'		=> 'manage_group_initialise_data',
-			'core.acp_manage_group_display_form' 		=> 'manage_group_display_form',
-			'core.viewtopic_modify_post_row' 			=> 'modify_post_row',
-			'core.memberlist_prepare_profile_data'		=> 'profile_template',
+			'core.acp_users_overview_before'		=> 'acp_verified_member',
+			'core.acp_manage_group_request_data'	=> 'add_group',
+			'core.acp_manage_group_initialise_data'	=> 'manage_group_initialise_data',
+			'core.acp_manage_group_display_form' 	=> 'manage_group_display_form',
+			'core.memberlist_prepare_profile_data'	=> 'profile_template',
+			'core.modify_username_string'			=> 'modify_username',
 		];
+	}
+
+	/**
+	* Show whether a member is verified on ACP overview
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function acp_verified_member($event)
+	{
+		$this->language->add_lang('acp_users', $this->functions->get_ext_namespace());
+
+		$user_row 		= $event['user_row'];
+		$verify_image	= $this->get_group_image($user_row['user_id']);
+
+		$this->template->assign_vars([
+			'GROUP_NAME'		=> ($verify_image) ? $this->group_helper->get_name($verify_image['group_name']) : '',
+
+			'SHOW_VERIFY_IMAGE'	=> ($verify_image) ? true : false,
+
+			'VERIFIED_MEMBER' 	=> ($verify_image) ? $this->language->lang('Yes') : $this->language->lang('NO'),
+			'VERIFY_IMAGE' 		=> ($verify_image) ? $this->images_path . '/' . $verify_image['group_verified_member'] : '',
+		]);
 	}
 
 	/**
@@ -96,9 +127,9 @@ class listener implements EventSubscriberInterface
 	 */
 	public function add_group($event)
 	{
-		$submit_ary = $event['submit_ary'];
-		$submit_ary['verified_member'] = $this->request->variable('group_verified_member', '');
-		$event['submit_ary'] = $submit_ary;
+		$submit_ary 					= $event['submit_ary'];
+		$submit_ary['verified_member']	= $this->request->variable('group_verified_member', '');
+		$event['submit_ary'] 			= $submit_ary;
 	}
 
 	/**
@@ -132,14 +163,15 @@ class listener implements EventSubscriberInterface
 	{
 		$this->language->add_lang('acp_groups', $this->functions->get_ext_namespace());
 
-		$group_row = $event['group_row'];
-		$group_row['group_verified_member'] = (!empty($group_row)) ? $group_row['group_verified_member'] : '';
+		$group_row 							= $event['group_row'];
+		$group_row['group_verified_member']	= (!empty($group_row)) ? $group_row['group_verified_member'] : '';
 
-		$files = array_slice(scandir($this->images_path), 2);
-
+		// Create the select list from the images folder
 		$image_files	= '';
+		$files 			= array_slice(scandir($this->images_path), 2);
 		$selected    	= ($group_row['group_verified_member'] == '') ? ' selected="selected"' : '';
 		$image_files 	.= '<option value="' . '' . '"' . $selected . '>' . $this->language->lang('SELECT_IMAGE') . '</option>';
+
 		foreach ($files as $image)
 		{
 			$selected 		= ($group_row['group_verified_member'] == $image) ? ' selected="selected"' : '';
@@ -148,60 +180,65 @@ class listener implements EventSubscriberInterface
 		$image_select = '<select name="group_verified_member" id="group_verified_member">' . $image_files . '</select>';
 
 		$this->template->assign_vars([
+			'SHOW_VERIFY_IMAGE'	=> ($group_row['group_verified_member']) ? true : false,
+
 			'VERIFIED_MEMBER' 	=> $image_select,
 			'VERIFY_IMAGE' 		=> $this->images_path . '/' . $group_row['group_verified_member'],
-			'SHOW_VERIFY_IMAGE'	=> ($group_row['group_verified_member']) ? true : false,
 		]);
 	}
 
 	/**
-	 * Modify the post row
-	 *
-	 * @return  $event
-	 * @access  public
-	 */
-	public function modify_post_row($event)
-	{
-		$post_row 						= $event['post_row'];
-		$verify_image 					= $this->get_group_image($post_row['POSTER_ID']);
-		$post_row['VERIFY_IMAGE'] 		= ($verify_image) ? $this->images_path . '/' . $verify_image['group_verified_member'] : '';
-		$post_row['SHOW_VERIFY_IMAGE']	= ($verify_image) ? true : false;
-		$event['post_row'] 				= $post_row;
-	}
-
-	/**
 	 * Modify the profile template data
+	 * This is needed to prevent problems with Memberlist popup
 	 *
 	 * @return  $event
 	 * @access  public
 	 */
 	public function profile_template($event)
 	{
-		$template_data 						= $event['template_data'];
-		$data 								= $event['data'];
-		$verify_image 						= $this->get_group_image($data['user_id']);
-		$template_data['VERIFY_IMAGE'] 		= ($verify_image) ? $this->images_path . '/' . $verify_image['group_verified_member'] : '';
-		$template_data['SHOW_VERIFY_IMAGE']	= ($verify_image) ? true : false;
-		$event['template_data'] 			= $template_data;
+		$template_data 					= $event['template_data'];
+		$data 							= $event['data'];
+		$template_data['A_USERNAME']	= addslashes(get_username_string('username', $data['user_id'], $data['username'], $data['user_colour'], false, true));
+		$event['template_data'] 		= $template_data;
+	}
+
+	/**
+	 * Modify the username string
+	 *
+	 * @return  $event
+	 * @access  public
+	 */
+	public function modify_username($event)
+	{
+		$username_string 	= $event['username_string'];
+		$mode 				= $event['mode'];
+		$verify_image 		= $this->get_group_image($event['user_id']);
+
+		if ($verify_image && ($mode == 'full' || ($mode == 'username' && !$event['custom_profile_url'])))
+		{
+			$username_string = $username_string . '&nbsp;<img src="' . $this->images_path . '/' . $verify_image['group_verified_member'] . '" />';
+		}
+
+		$event['username_string'] = $username_string;
 	}
 
 	/**
 	 * Get the group image for the user
 	 *
-	 * @return  $event
+	 * @return  $image_data
 	 * @access  public
 	 */
 	public function get_group_image($user_id)
 	{
 		$sql = $this->db->sql_build_query('SELECT', [
-			'SELECT' => 'g.group_verified_member',
-			'FROM' => [
+			'SELECT'	=> 'g.group_verified_member, g.group_name',
+			'FROM' 		=> [
 				$this->tables['groups'] => 'g',
 			],
 			'LEFT_JOIN' => [
 				[
-					'FROM' => [$this->tables['user_group'] => ' ug', ],
-					'ON' => 'ug.group_id = g.group_id',
+					'FROM'	=> [$this->tables['user_group'] => ' ug', ],
+					'ON' 	=> 'ug.group_id = g.group_id',
 				],
 			],
 			'WHERE' => 'ug.user_id = ' . (int) $user_id,
